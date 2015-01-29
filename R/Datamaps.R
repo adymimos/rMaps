85951d56e1362f0f246c171784fe7afb03638e7d
adymimos <adline.dsilva@mimos.my>
2015-01-29 14:48:56 +0800

    Update Datamaps.R

--- R/Datamaps.R
+++ R/Datamaps.R
@@ -48,8 +48,8 @@ ichoropleth <- function(x, data, pal = "Blues", ncuts = 5, eff=FALSE, animate = 
     data = transform(data, 
       fillKey = cut(
         fml$left, 
-        #quantile(c(100,fml$left[which(fml$left!=100)]),seq(0, 1, 1/ncuts)),
-        unique(quantile(fml$left, seq(0, 1, 1/ncuts))), quantile(fml$left, seq(0, 1, 1/ncuts)),
+        quantile(c(100,fml$left[which(fml$left!=100)]),seq(0, 1, 1/ncuts)),
+        
         ordered_result = TRUE
       )
     )
@@ -59,7 +59,7 @@ ichoropleth <- function(x, data, pal = "Blues", ncuts = 5, eff=FALSE, animate = 
     data = transform(data, 
       fillKey = cut(
         fml$left, 
-        quantile(fml$left, seq(0, 1, 1/ncuts)),
+        unique(quantile(fml$left, seq(0, 1, 1/ncuts))), quantile(fml$left, seq(0, 1, 1/ncuts)),
         ordered_result = TRUE
       )
     )
